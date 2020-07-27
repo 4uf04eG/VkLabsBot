ccced160165e6253ff67b5bcb69f58faa5ea944f
Илья Курин <4uf04eg@gmail.com>
2020-07-27 18:55:02 +0400

    Deploy fixes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,22 @@
 package main.kotlin
 
-fun main() {
-    val groupId: Int = 197339206
-    val vkAccessToken: String = System.getenv("VK_ACCESS_TOKEN")
-    val dropboxAccessToken: String = System.getenv("DROPBOX_ACCESS_TOKEN")
+import java.nio.file.Files
+import java.nio.file.Paths
 
-    val storage = CloudStorage(dropboxAccessToken)
-    val bot = LabsBot(groupId, vkAccessToken, storage)
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val groupId: Int = 197339206
+            val vkAccessToken: String = System.getenv("VK_ACCESS_TOKEN")
+            val dropboxAccessToken: String = System.getenv("DROPBOX_ACCESS_TOKEN")
 
-    bot.startLongPolling()
+            Files.createDirectories(Paths.get("files"))
+
+            val storage = CloudStorage(dropboxAccessToken)
+            val bot = LabsBot(groupId, vkAccessToken, storage)
+
+            bot.startLongPolling()
+        }
+    }
 }
